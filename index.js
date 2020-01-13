bf695e388da90b16bdd979839129b27fce62ba7c
Hanieh Marvi <haniemarvi@gmail.com>
2020-01-13 15:58:51 -0330

    first pop-up

--- index.js
+++ index.js
@@ -8,6 +8,9 @@ var OpenTopoMap = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png'
     attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
 });
 
+var myDataPoint = L.marker([47.575232, -52.732506]).addTo(map);
+myDataPoint.bindPopup("<p> <h3> Memorial University of Newfoundland </h3> <br> St. John's, CA<br><a href='https://www.mun.ca/'>MUN Website</a><br/> </p>");
+
 OpenTopoMap.addTo(map)
 
 //Fetching .json file using URL
