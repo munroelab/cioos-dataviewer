9a2f45e8586bdda2ee6c6aacd28576f03d4c1cd2
James Munroe <jmunroe@mun.ca>
2020-01-13 15:27:54 -0330

    Switch to OpenTopoMap and set default zoom level to 5

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
-var map = L.map('map',{ center: [47.570608, -52.720862], zoom: 10});
+var map = L.map('map',{ center: [47.570608, -52.720862], zoom: 5});
 
-    	// Add OpenStreetMap tile layer to map element
-        L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png',
-         { attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>' }).addTo(map);
+var OpenTopoMap = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png',
+ {
+	maxZoom: 17,
+	attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
+});
+
+OpenTopoMap.addTo(map)
