6c87f40f83e1553086c40c9003dd8946c51a55ac
aidaghayour <aghayourkhia@mun.ca>
2020-02-28 15:58:01 -0330

    Access through API

--- cards.js
+++ cards.js
@@ -2,10 +2,10 @@
 
 //var ckanapi = "https://cioosatlantic.ca/ckan/api/3/action/"
 //var query = ckanapi + "package_list"
-var query = "./package_list.json"
-fetch(query)
-.then(res => {return res.json})
-.then(da => {return console.log(da.help)});
+// var query = "https://cioosatlantic.ca/ckan/api/3/action/package_list.json"
+// fetch(query)
+// .then(res => {return res.json})
+// .then(da => {return console.log(da.help)});
 //https://cioosatlantic.ca/ckan/api/3/action/package_show?id=sma_st_johns
 const app = document.getElementById('root');
 
@@ -19,32 +19,32 @@ app.appendChild(logo);
 app.appendChild(container);
 
 var request = new XMLHttpRequest();
-request.open('GET', 'package_list.json', true);
+request.open('GET', 'https://cioosatlantic.ca/ckan/api/3/action/package_list', true);
 request.onload = function () {
 
   // Begin accessing JSON data here
   var data = JSON.parse(this.response);
   if (request.status >= 200 && request.status < 400) {
     data.result.forEach(station => {
-            fetch("./"+"sma_saint_john_wharf")
+            fetch("https://cioosatlantic.ca/ckan/api/3/action/package_show?id=" + station)
             .then(resp => resp.json())
             .then(dt => JSON.parse(JSON.stringify(dt)))
-            .then(dat => console.log(dat.help));
+            .then(dat => 
             //console.log()
 
-            const card = document.createElement('div');
+            {const card = document.createElement('div');
             card.setAttribute('class', 'card');
 
             const h1 = document.createElement('h1');
-            h1.textContent = station;
+            h1.textContent = "Station Name:\n" + station;
 
             const p = document.createElement('p');
-            station.description = station.substring(0, 3);
-            p.textContent = `${"Information about this station will appear here!"}...`;
+            p.textContent = `${dat.result.notes_translated.en.substring(0, 300)}...`;
 
             container.appendChild(card);
             card.appendChild(h1);
             card.appendChild(p);
+            console.log(dat.result.notes_translated.en)});
     });
   } else {
     const errorMessage = document.createElement('marquee');
